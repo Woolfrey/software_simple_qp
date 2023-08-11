8bf3e1abc69da9284ecb65bd91ecfa271163bbba
Jon <jonathan.woolfrey@gmail.com>
2023-08-11 07:48:41 +0200

    Fixed up some formatting. Constrained least squares working.

--- src/test.cpp
+++ src/test.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	          << "   - H (nxn) is a positive semi-definite matrix such that H = H', and\n"
 	          << "   - f (nx1) is a vector for the linear component of the quadratic equation.\n"
 	          << "\nFor this type of problem is is possible to call the class method without creating "
-	          << "an object. For example, here is the H matrix (Hessian) for a " << m << "x" << n << " system:\n";
+	          << "an object.\n For example, here is the H matrix (Hessian) for a " << m << "x" << n << " system:\n";
 	
 	std::cout << "\n" << H << std::endl;
 	
@@ -63,15 +63,15 @@ int main(int argc, char *argv[])
 	A = Eigen::MatrixXf::Random(m,n);
 	y = A*Eigen::VectorXf::Random(n);
         
-	std::cout << "\nThe QP solver can be used to minimize problems of the form:\n"
+	std::cout << "\nThe QP solver can be used to minimize classic least squares / regression "
+	          << "problems of the form:\n"
 	          << "\n     min 0.5*|| y - A*x ||^2 = 0.5*(y - A*x)'*W*(y - A*x)\n"
 	          << "\nwhere:\n"
 	          << " - y (mx1) is given,\n" 
 	          << " - A (mxn) is also given,\n"
 	          << " - W (mxm) is a weighting matrix, and\n"
 	          << " - x (nx1) is the decision variable.\n"
-	          << "\nIt is assumed that m > n so the equations are underdetermined. "
-	          << "This is a classic linear regression / linear least squares problem.\n";
+	          << "\nIt is assumed that m > n so the equations are underdetermined.\n";
 	
 	std::cout << "\nFor example, here is a " << m << "x" << n << " system where A is:\n";
 	
@@ -114,7 +114,7 @@ int main(int argc, char *argv[])
 	std::cout << "\n" << y.transpose() << std::endl;
 	         
 	std::cout << "\nSince m < n there are infinite possible solutions. We can give the QP solver a "
-	          <<   "desired value xd for the solution. This problem takes the form:\n";
+	          <<   "desired value xd for the solution.\n This problem takes the form:\n";
 	          
 	std::cout << "\n      min 0.5*(xd - x)'*W*(xd - x)\n"
 	          <<   "     subject to: A*x = y\n";
@@ -138,11 +138,11 @@ int main(int argc, char *argv[])
 	          <<   "*                      CONSTRAINED SYSTEMS                           *\n"
 	          <<   "**********************************************************************\n" << std::endl;
 	          
-	m = 6;
-	n = 5;
+	m = 9;
+	n = 6;
 	
 	A = Eigen::MatrixXf::Random(m,n);
-	x.resize(n); x << -3, 5, -1, 4, 0;
+	x.resize(n); x << -3, 5, -1, 6, 0, -2;
 	y = A*x;
 	
 	xMin = -5*Eigen::VectorXf::Ones(n);
@@ -163,8 +163,8 @@ int main(int argc, char *argv[])
                   << "\n     subject to: xMin <= x <= xMax\n"
                   << "\nwhere xMin (nx1) and xMax(nx1) are lower and upper bounds on the solution.\n"
                   << "\nThe equivalent constraints here are:\n"
-                  << "\n    B = [  I ]  z = [ xMax ]\n"
-                  <<   "        [ -I ]      [ xMin ]\n"
+                  << "\n    B = [  I ]  z = [  xMax ]\n"
+                  <<   "        [ -I ]      [ -xMin ]\n"
                   << "\n(I wrote special function for this case because I am lazy.)\n";     
                   
         std::cout << "\nFor the following system of A:\n";
@@ -191,6 +191,9 @@ int main(int argc, char *argv[])
 
 	std::cout << "\nThe error ||y - A*x|| is: " << (y - A*x).norm() << ", "
 	          <<   "and it took " << t*1000 << " ms to solve (" << 1/t << " Hz).\n";
+	          
+	std::cout << "\nThere is signicant error because the real solution lies outside the constraints.\n"
+	          << "BUT, the QP solver is able to satisfy them!\n";
 	          
 
 	std::cout << "\n**********************************************************************\n"
@@ -221,7 +224,7 @@ int main(int argc, char *argv[])
 	std::cout << "\nWe would call: 'solver.constrained_least_squares(xd,W,A,y,xMin,xMax,x0)'\n"
 	          << "\nThere are two options for this case:\n"
 	          << "\n  1. solver.use_dual() which is faster, but sensitive to initial guess x0, and\n"
-	          <<   "  2. solver.use_primal() which is slower, but robust.\n";
+	          <<   "  2. solver.use_primal() which is slower, but (usually) more robust.\n";
 	
 	std::cout << "\nHere is the solution for a " << m << "x" << n << " system using the dual method:\n";
 	
@@ -260,153 +263,6 @@ int main(int argc, char *argv[])
 	          
 	std::cout << "\nThe dual method was " << t2/t1 << " times faster. "
 	          << "The primal method was " << error1/error2 << " times more accurate.\n";
-	
-/*	srand((unsigned int) time(0));					                            // Random seed generator
-	
-	// Variables used in this scope
-	int m, n;
-	Eigen::MatrixXf A, comparison, W;
-	Eigen::VectorXf x, xd, xHat, x0, xMin, xMax, y;
-	QPSolver solver;
-	clock_t timer;
-	float time;
-	
-	std::cout << "\n************************************************************\n"
-	          <<   "*                  UNDERDETERMINED SYSTEMS	           *\n"
-	          <<   "************************************************************\n" << std::endl;
 	          
-	m = 6;
-	n = 5;
-	A  = Eigen::MatrixXf::Random(m,n);
-	x  = Eigen::VectorXf::Random(n);
-	y  = A*x;
-	W  = Eigen::MatrixXf::Identity(m,m);
-	
-	std::cout << "\nHere is an underdetermined system y = A*x.\n" << std::endl;
-	
-	std::cout << "\nA:\n" << std::endl;
-	std::cout << A << std::endl;
-	
-	std::cout << "\ny:\n" << std::endl;
-	std::cout << y << std::endl;
-	
-	std::cout << "\nWe can use quadratic programming (QP) to get the best estimate of x.\n" << std::endl;
-	
-	timer = clock();
-	xHat  = QPSolver::least_squares(y,A,W);
-	timer = clock() - timer;
-	time  = (float)timer/CLOCKS_PER_SEC;
-	
-	std::cout << "\nHere is the estimate for x:\n" << std::endl;
-	std::cout << xHat << std::endl;
-	
-	std::cout << "\nThe error ||y-A*x|| is " << (y-A*xHat).norm() << ". "
-	          << "It took " << time*1000 << " ms to solve (" << 1/time << " Hz).\n" << std::endl;
-	          
-	std::cout << "\n************************************************************\n"
-	          <<   "*                    CONSTRAINED SYSTEM                    *\n"
-	          <<   "************************************************************\n" << std::endl;
-	
-	m = 8;
-	n = 8;
-	
-	A = Eigen::MatrixXf::Random(m,n);
-	x.resize(n);
-	x << 1, 2, 3, 4, 5, 3, 2, 1;
-	
-	y = A*x;
-	W = Eigen::MatrixXf::Identity(m,m);
-	
-	xMin = -6*Eigen::VectorXf::Ones(n);
-	xMax =  6*Eigen::VectorXf::Ones(n);
-	xMax(2) = 0.9*x(2);
-	xMax(3) = 0.9*x(3);
-	
-	x0   = 0.5*(xMin + xMax);
-	
-	std::cout << "\nConsider the problem to minimize ||y-A*x|| for xMin <= x <= xMax.\n" << std::endl;
-	
-	std::cout << "\nHere is A:\n" << std::endl;
-	std::cout << A << std::endl;
-	
-	std::cout << "\nand y:\n" << std::endl;
-	std::cout << y << std::endl;
-	
-	timer = clock();
-	xHat  = solver.least_squares(y,A,W,xMin,xMax,x0);
-	timer = clock() - timer;
-	time  = (float)timer/CLOCKS_PER_SEC;
-	
-	std::cout << "\nHere is xMin, the estimate for x, and xMax side-by-side:\n" << std::endl;
-	comparison.resize(n,3);
-	comparison.col(0) = xMin;
-	comparison.col(1) = xHat;
-	comparison.col(2) = xMax;
-	std::cout << comparison << std::endl;
-	
-	std::cout << "\nThe error norm ||y-A*x|| is " << (y-A*xHat).norm() << ". "
-	          << "It took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
-        
-	std::cout << "\n************************************************************\n"
-	          <<   "*                 OVERDETERMINED SYSTEMS                   *\n"
-	          <<   "************************************************************\n" << std::endl;
-	m = 12;
-	n = 17;
-
-	x = Eigen::VectorXf::Random(n);
-	A = Eigen::MatrixXf::Random(m,n);
-	y = A*x;
-	
-	xMin = -5*Eigen::VectorXf::Ones(n);
-	xMax =  5*Eigen::VectorXf::Ones(n);
-	
-	x0 = 0.5*(xMin + xMax);
-	
-	xd = 10*Eigen::VectorXf::Random(n);
-	
-	try
-	{
-		timer = clock();
-		xHat  = solver.redundant_least_squares(xd,Eigen::MatrixXf::Identity(n,n),y,A,xMin,xMax,x0);
-		timer = clock() - timer;
-		time  = (float)timer/CLOCKS_PER_SEC;
-	
-		std::cout << "\nHere is xMin, x, xHat, and xMax:\n" << std::endl;
-		comparison.resize(n,3);
-		comparison.col(0) = xMin;
-		comparison.col(1) = xHat;
-		comparison.col(2) = xMax;
-		std::cout << comparison << std::endl;
-
-		std::cout << "\nThe error norm ||y - A*x|| is " << (y-A*xHat).norm() << ". "
-			  << "It took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
-	}
-	catch(const std::exception &exception)
-	{
-		std::cout << exception.what() << std::endl;
-	}
-
-	// Record statistics on performance
-	std::ofstream out("qp_test_data.csv");
-	for(int i = 0; i < 100; i++)
-	{
-		x = Eigen::VectorXf::Random(n);
-		A = Eigen::MatrixXf::Random(m,n);
-		y = A*x;
-		
-		xMin = -5*Eigen::VectorXf::Ones(n);
-		xMax =  5*Eigen::VectorXf::Ones(n);
-		
-		x0 = 0.5*(xMin + xMax);
-		
-		xd = 10*Eigen::VectorXf::Random(n);
-		
-		timer = clock();
-		xHat  = solver.redundant_least_squares(xd,Eigen::MatrixXf::Identity(n,n),y,A,xMin,xMax,x0);
-		timer = clock() - timer;
-		
-		out << (float)timer/CLOCKS_PER_SEC << "," << (y-A*xHat).norm() << "\n";
-	}
-*/
 	return 0; 
 }
